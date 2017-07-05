547f0947c550d222577497b159d109c84c7f3e48
Fernando Medina Corey <fmcorey@gmail.com>
2017-07-05 14:43:44 -0700

    Forked all code from dead serverless repo

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # serverless-finch
-A Serverless Framework Plugin for Static Site Deployment
+
+A Serverless Framework plugin for deployment of static website assests of your Serverless project to AWS S3.
+
+Forked from the **serverless-client-s3**
+https://github.com/serverless/serverless-client-s3/
+
+**First**, install:
+
+```
+npm install --save serverless-finch
+```
+
+**Second**, update `serverless.yml` by adding the following:
+
+```yaml
+plugins:
+  - serverless-finch
+
+custom:
+  client:
+  bucketName: unique-s3-bucketname-for-your-website-files
+```
+
+* **Warning:** The plugin will overwrite any data you have in the bucket name you set above if it already exists.
+
+
+**Third**, Create a `client/dist` folder in the root directory of your Serverless project. This is where your distribution-ready website should live. 
+
+The plugin simply uploads the entire `client/dist` folder to S3 and configures the bucket to host the website and make it publicly available.
+
+To test the plugin initially you can copy/run the following commands in the root directory of your Serverless project to get a quick sample website for deployment:
+
+```
+mkdir -p client/dist
+touch client/dist/index.html
+touch client/dist/error.html
+echo "Go Serverless" >> client/dist/index.html
+echo "error page" >> client/dist/error.html
+```
+
+**Fourth**, run the plugin, and visit your new website!
+
+```
+serverless client deploy [--stage $STAGE] [--region $REGION]
+```
+
+**Fifth**, Have fun!
---
+++ index.js
@@ -0,0 +1,226 @@
+'use strict';
+
+const path     = require('path');
+const BbPromise    = require('bluebird');
+const async        = require('async');
+const _            = require('lodash');
+const mime         = require('mime');
+const fs           = require('fs');
+
+class Client {
+  constructor(serverless, options){
+    this.serverless = serverless;
+    this.stage = options.stage || _.get(serverless, 'service.provider.stage')
+    this.region = options.region || _.get(serverless, 'service.provider.region');
+    this.provider = 'aws';
+    this.aws = this.serverless.getProvider(this.provider);
+
+    this.commands = {
+      client: {
+        usage: 'Generate and deploy clients',
+        lifecycleEvents:[
+          'client',
+          'deploy'
+        ],
+        commands: {
+          deploy: {
+            usage: 'Deploy serverless client code',
+            lifecycleEvents:[
+              'deploy'
+            ]
+          }
+        }
+      }
+    };
+
+
+    this.hooks = {
+      'client:client': () => {
+        this.serverless.cli.log(this.commands.client.usage);
+      },
+
+      'client:deploy:deploy': () => {
+        this._validateAndPrepare()
+          .then(this._processDeployment.bind(this));
+      }
+    };
+  }
+
+  _validateAndPrepare() {
+    const Utils = this.serverless.utils;
+    const Error = this.serverless.classes.Error;
+
+    const _dist = _.get(this.serverless, 'service.custom.client.distributionFolder', 'dist');
+
+    if (!Utils.dirExistsSync(path.join(this.serverless.config.servicePath, 'client', _dist))) {
+      return BbPromise.reject(new Error('Could not find "client/' + _dist + ' folder in your project root.'));
+    }
+
+    if (!this.serverless.service.custom ||
+        !this.serverless.service.custom.client ||
+        !this.serverless.service.custom.client.bucketName) {
+      return BbPromise.reject(new Error('Please specify a bucket name for the client in serverless.yml.'));
+    }
+
+    this.bucketName = this.serverless.service.custom.client.bucketName;
+    this.clientPath = path.join(this.serverless.config.servicePath, 'client', _dist);
+
+    return BbPromise.resolve();
+  }
+
+
+  _processDeployment() {
+    this.serverless.cli.log('Deploying client to stage "' + this.stage + '" in region "' + this.region + '"...');
+
+
+    function listBuckets(data) {
+      data.Buckets.forEach(function(bucket) {
+        if (bucket.Name === this.bucketName) {
+          this.bucketExists = true;
+          this.serverless.cli.log(`Bucket ${this.bucketName} already exists`);
+        }
+      }.bind(this));
+    }
+
+    function listObjectsInBucket() {
+      if (!this.bucketExists) return BbPromise.resolve();
+
+      this.serverless.cli.log(`Listing objects in bucket ${this.bucketName}...`);
+
+      let params = {
+        Bucket: this.bucketName
+      };
+
+      return this.aws.request('S3', 'listObjectsV2', params, this.stage, this.region);
+    }
+
+    function deleteObjectsFromBucket(data) {
+      if (!this.bucketExists) return BbPromise.resolve();
+
+      this.serverless.cli.log(`Deleting all objects from bucket ${this.bucketName}...`);
+
+      if (!data.Contents[0]) {
+        return BbPromise.resolve();
+      } else {
+        let Objects = _.map(data.Contents, function (content) {
+          return _.pick(content, 'Key');
+        });
+
+        let params = {
+          Bucket: this.bucketName,
+          Delete: { Objects: Objects }
+        };
+
+        return this.aws.request('S3', 'deleteObjects', params, this.stage, this.region);
+      }
+    }
+
+    function createBucket() {
+      if (this.bucketExists) return BbPromise.resolve();
+      this.serverless.cli.log(`Creating bucket ${this.bucketName}...`);
+
+      let params = {
+        Bucket: this.bucketName
+      };
+
+      return this.aws.request('S3', 'createBucket', params, this.stage, this.region)
+    }
+
+    function configureBucket() {
+      this.serverless.cli.log(`Configuring website bucket ${this.bucketName}...`);
+
+      let params = {
+        Bucket: this.bucketName,
+        WebsiteConfiguration: {
+          IndexDocument: { Suffix: 'index.html' },
+          ErrorDocument: { Key: 'error.html' }
+        }
+      };
+
+      return this.aws.request('S3', 'putBucketWebsite', params, this.stage, this.region)
+    }
+
+    function configurePolicyForBucket(){
+      this.serverless.cli.log(`Configuring policy for bucket ${this.bucketName}...`);
+
+      let policy = {
+        Version: "2008-10-17",
+        Id: "Policy1392681112290",
+        Statement: [
+          {
+            Sid: "Stmt1392681101677",
+            Effect: "Allow",
+            Principal: {
+              AWS: "*"
+            },
+            Action: "s3:GetObject",
+            Resource: "arn:aws:s3:::" + this.bucketName + '/*'
+          }
+        ]
+      };
+
+      let params = {
+        Bucket: this.bucketName,
+        Policy: JSON.stringify(policy)
+      };
+
+      return this.aws.request('S3', 'putBucketPolicy', params, this.stage, this.region);
+    }
+
+    return this.aws.request('S3', 'listBuckets', {}, this.stage, this.region)
+      .bind(this)
+      .then(listBuckets)
+      .then(listObjectsInBucket)
+      .then(deleteObjectsFromBucket)
+      .then(createBucket)
+      .then(configureBucket)
+      .then(configurePolicyForBucket)
+      .then(function(){
+        return this._uploadDirectory(this.clientPath)
+      });
+  }
+
+  _uploadDirectory(directoryPath) {
+    let _this         = this,
+    readDirectory = _.partial(fs.readdir, directoryPath);
+
+    async.waterfall([readDirectory, function (files) {
+      files = _.map(files, function(file) {
+        return path.join(directoryPath, file);
+      });
+
+      async.each(files, function(path) {
+        fs.stat(path, _.bind(function (err, stats) {
+
+          return stats.isDirectory()
+            ? _this._uploadDirectory(path)
+            : _this._uploadFile(path);
+        }, _this));
+      });
+    }]);
+  }
+
+  _uploadFile(filePath) {
+    let _this      = this,
+        fileKey    = filePath.replace(_this.clientPath, '').substr(1).replace(/\\/g, '/');
+
+    this.serverless.cli.log(`Uploading file ${fileKey} to bucket ${_this.bucketName}...`);
+
+    fs.readFile(filePath, function(err, fileBuffer) {
+
+      let params = {
+        Bucket: _this.bucketName,
+        Key: fileKey,
+        Body: fileBuffer,
+        ContentType: mime.lookup(filePath)
+      };
+
+      // TODO: remove browser caching
+      return _this.aws.request('S3', 'putObject', params, _this.stage, _this.region);
+    });
+
+  }
+
+}
+
+module.exports = Client;
